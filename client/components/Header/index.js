060603ea89f1c87ada0768aecccd9822071c8ec4
Sajid Jaber <sajidjaber@yahoo.com>
2022-12-29 12:47:54 -0600

    navbar started

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import styled from "styled-components";
+import Image from "next/image"
+import Logo from "../../public/assets/images/logo2.png"
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const Container = styled.div`
+    padding: 10px;
+  `;
+  const Nav = styled.nav`
+      display: flex;
+    justify-content: space-between;
+    text-align: center;
+    align-items: center;
+  `;
+  const Left = styled.div`
+    flex: 1;
+  `;
+  const LogoNav = styled(Image)`
+  width: 20vw;
+  height: 10vw;
+  `
 
-export default Header
+
+
+  const Center = styled.div`
+    flex: 1;
+  `;
+  const Right = styled.div`
+    flex: 1;
+  `;
+
+  return (
+    <Container>
+      <Nav>
+        <Left>L</Left>
+        <Center><LogoNav src={Logo} /></Center>
+        <Right>Right</Right>
+      </Nav>
+    </Container>
+  );
+};
+
+export default Header;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,14 @@
 
 
 import Layout from '../components/layout'
-
+import { ThemeProvider } from 'next-themes'
 const MyApp = ({ Component, pageProps }) => {
   return (
+    <ThemeProvider>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </ThemeProvider>
   )
 }
 
